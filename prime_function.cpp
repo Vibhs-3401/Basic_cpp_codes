4bb0dd60d0350a4d59125a62393ec1d7edc5f8f1
Vibhuti Patel <72439885+Vibhs-3401@users.noreply.github.com>
2021-07-29 22:08:04 +0530

    Create prime_function.cpp

---
+++ prime_function.cpp
@@ -0,0 +1,27 @@
+// take 2 number from user, keep it as a range and print out all prime numbers in the range.
+
+
+#include <iostream>
+#include <math.h>
+using namespace std;
+
+bool isPrime(int num) {
+    for (int i=2; i<=sqrt(num); i++) {
+        if (num%i == 0) return false;
+    }
+    return true;
+}
+
+int main() {
+
+    int a,b;
+    cin>>a>>b;
+    
+    for (int i=a; i<b; i++) {
+        if (isPrime(i)) {
+            cout<<i<<endl;
+        }
+    }
+
+    return 0;
+}
